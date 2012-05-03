c0b35b6760db8b898677fd43fa59b989d74498a1
Drew Young <drewyoung1@gmail.com>
2012-05-03 15:42:11 -0400

    Add simple bootloader based on require.js

---
+++ bootloader/filesystem/boot/main.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+	var fs = require('FS');
+	var api = require('API');
+	
+	console.log(fs, api);
+	
+	api();
+});
---
+++ bootloader/index.js
@@ -0,0 +1,21 @@
+define(['require', 'exports', '../index', './bootloader'], function(require, exports, fs, bootLoader) {
+	var Filesystem = fs.Filesystem;
+	var BootLoader = bootLoader.BootLoader;
+	
+	exports.testFs = new Filesystem({
+		root: {
+			files: {
+				
+			}
+		}
+	});
+	
+	exports.bootLoader = new BootLoader(exports.testFs, function() {
+		console.log('called api');
+	}, {
+		baseUrl: 'filesystem'
+	});
+	
+	window.testFs = exports.testFs;
+	window.bootLoader = exports.bootLoader;
+});
